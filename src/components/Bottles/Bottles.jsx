9afe9be37d92bab63bfb369856ffecdcc04e992e
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-23 23:49:43 +0600

    added the button data storege

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -5,18 +5,33 @@ import './Bottles.css'
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
+   const [cart,setCart]=useState([]); 
+
+
   useEffect(() => {
     fetch("bottles.json")
       .then((res) => res.json())
       .then((data) => setBottles(data));
   }, []);
 
+
+const handleAddToCart=bottle =>{
+    const newCart =[...cart,bottle] 
+    setCart(newCart); 
+ }
+
+
   return (
     <div>
-      <h2>Bottles Here:{bottles.length}</h2>
-         <div className="bottle-container">
+      <h2>Bottles Available:{bottles.length}</h2>
+      <h4>Cart:{cart.length}</h4>
+          <div className="bottle-container">
          {
-        bottles.map(bottle => <Bottle bottle={bottle} key={bottle.id} ></Bottle>)
+        bottles.map(bottle => <Bottle
+             bottle={bottle}
+              key={bottle.id}
+              handleAddToCart={handleAddToCart}
+               ></Bottle>)
        }
 
          </div>
