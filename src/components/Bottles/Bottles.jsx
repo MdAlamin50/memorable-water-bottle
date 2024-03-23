d7039f65bac75dcdb990c61ff03723b2600cebe0
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-03-24 05:23:23 +0600

    added the cart commit

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -15,13 +15,23 @@ const Bottles = () => {
   }, []);
 
   // load cart from local storage
-  useEffect(()=>{
-    console.log('called the useEffect', bottles.length)
-     if(bottles.length>0){
+  useEffect(() => {
+    console.log("called the useEffect", bottles.length);
+    if (bottles.length) {
       const storedCart = getStoredCart();
-      console.log(storedCart); 
-     }
-  },[bottles])
+      console.log(storedCart, bottles);
+      const savedCart = [];
+      for (const id of storedCart) {
+        console.log(id);
+        const bottle = bottles.find((bottle) => bottle.id === id);
+        if (bottle) {
+          savedCart.push(bottle);
+        }
+      }
+      console.log("saved cart", savedCart);
+      setCart(savedCart);
+    }
+  }, [bottles]);
 
   const handleAddToCart = (bottle) => {
     const newCart = [...cart, bottle];
@@ -32,8 +42,9 @@ const Bottles = () => {
   return (
     <div>
       <h2>Bottles Available:{bottles.length}</h2>
-      <h4>Cart:{cart.length}</h4>
-      <div className="bottle-container">
+        <h4>cart:{cart.length}</h4>
+        
+       <div className="bottle-container">
         {bottles.map((bottle) => (
           <Bottle
             bottle={bottle}
